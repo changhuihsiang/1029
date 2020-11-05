c9c08a88ee2adf11e79c0018fcc3f602698fb70b
hui <ac123456dk@gmail.com>
2020-11-05 17:13:51 +0800

    test

--- loveworks.php
+++ loveworks.php
@@ -17,13 +17,51 @@
         header("Location: page003.php?lovemem=1");
     } else{
     require "includes/db.php";
-    $sqll = "INSERT INTO lovework( mid, wid, lovework) VALUES ('$user_type','$wid','$lovework')";
-    if ($conn->query($sqll) === TRUE) {
-        echo "成功";
-        header("Location: page003.php");
-      } else {
-        echo "Error: " . $sqll . "<br>" . $conn->error;
-      }
+
+    $s = "SELECT * FROM lovework WHERE mid='$user_type'and wid='$wid'";
+           $r = $conn->query($s);
+           if ($r->num_rows > 0) {
+             while($row = $r->fetch_assoc()) {
+               $nowlovework = $row["lovework"];
+                }
+            }
+
+               if($nowlovework==NULL){
+                    $sqll = "INSERT INTO lovework( mid, wid, lovework) VALUES ('$user_type','$wid','$lovework')";
+                    if ($conn->query($sqll) === TRUE) {
+                        echo "成功";
+                        header("Location: page003.php");
+                    } else {
+                        echo "Error: " . $sqll . "<br>" . $conn->error;
+                    }
+
+               } else{
+
+                    $sql = "UPDATE mem SET name='$name',username='$userid',password='$passchk',birth='$birth',phone='$phone' WHERE id='$id' ";
+
+                    $sqll = "UPDATE lovework SET lovework='$lovework' WHERE  mid='$user_type'and wid='$wid'";
+                    if ($conn->query($sqll) === TRUE) {
+                        echo "成功";
+                        header("Location: page003.php");
+                    } else {
+                        echo "Error: " . $sqll . "<br>" . $conn->error;
+                    }
+
+
+
+               }
+             
+           
+
+
+
+    // $sqll = "INSERT INTO lovework( mid, wid, lovework) VALUES ('$user_type','$wid','$lovework')";
+    // if ($conn->query($sqll) === TRUE) {
+    //     echo "成功";
+    //     header("Location: page003.php");
+    //   } else {
+    //     echo "Error: " . $sqll . "<br>" . $conn->error;
+    //   }
 
       $conn->close();
       }
